68483438fca684b6130be70262b829cf0bc6e991
Akigawa <sonoskeakigawa@gmail.com>
2019-10-31 15:07:23 -0700

    more

---
+++ GroceryList.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class GroceryList
+{
+  public static void Enter()
+  {
+    string[] myGroceryList = { "eggs", "milk", "bread", "bananas", "cereal", "rice", "Dad", "pizza", "dead rat", "chef boyardee's 'Food' bites" };
+
+    Console.WriteLine("My grocery list:");
+    for (int index = 0; index < myGroceryList.Length; index++)
+    {
+      Console.WriteLine(myGroceryList[index]);
+    }
+  }
+}
--- Lessons.cs
+++ Lessons.cs
@@ -4,10 +4,13 @@ using static Program;
 using static DoubleIt;
 using static RatedR;
 using static HotelCost;
+using static LeapYear;
+using static GroceryList;
 
 public class Lessons
 {
-    static int state = 0;
+    static string state = "";
+    static int lesson = 0;
     public static void Main()
     {
 
@@ -17,19 +20,49 @@ public class Lessons
     }
     public static void Projmenu()
     {
-        Console.WriteLine("Tell me which lesson you'd like!");
+
+        Console.WriteLine("Press a key to continue when the application pauses.");
+        Console.WriteLine("Ok?");
+        Console.ReadKey();
+        Console.WriteLine("Great! Here are the projects. Press a number and Press Enter to select one.");
         Console.WriteLine("1.) Hello world!");
         Console.WriteLine("Just says hi to ya.");
+        Console.ReadKey();
         Console.WriteLine("2.) Number Doubler!");
         Console.WriteLine("Returns double the number you put in.");
+        Console.ReadKey();
         Console.WriteLine("3.) MealTime!");
         Console.WriteLine("Asks what you had for meals, tells you what you told it after.");
+        Console.ReadKey();
         Console.WriteLine("4.)RatedR(Z?)");
         Console.WriteLine("Asks your name, then tells you whether or not there's a Z in it.");
+        Console.ReadKey();
         Console.WriteLine("5.) Hotel Cost");
         Console.WriteLine("Asks what season you're booking a hotel in and if you're booking on weekends.");
-        Lessons.state = System.Convert.ToInt32(Console.ReadLine());
-        switch (Lessons.state)
+        Console.ReadKey();
+        Console.WriteLine("6.) Leap Year");
+        Console.WriteLine("Tell the program your birth year and it will tell you how many leap years you've been alive.");
+        Console.ReadKey();
+        Console.WriteLine("6.) Leap Year");
+        Console.WriteLine("Returns a list of groceries.");
+        Lessons.state = Console.ReadLine();
+        if (Lessons.state != "" )
+        {
+            Lessons.lesson = System.Convert.ToInt32(Lessons.state);
+        }
+        Selection();
+    }
+    public static void Pause()
+    {
+        Console.WriteLine("...And that's the program. Press any key to continue back to the main menu.");
+        Console.ReadKey();
+        Lessons.Projmenu();
+        Console.Beep();
+    }
+    public static void Selection()
+    {
+
+        switch (Lessons.lesson)
         {
             case 1:
                 Program.Enter();
@@ -46,12 +79,16 @@ public class Lessons
             case 5:
                 HotelCost.Enter();
                 break;
+            case 6:
+                LeapYear.Enter();
+                break;
+            case 7:
+                GroceryList.Enter();
+                break;
+            default:
+                Console.WriteLine("What?");
+                Projmenu();
+                break;
         }
-
-    }
-    public static void Pause(){
-      Console.WriteLine("...And that's the program. Press any key to continue back to the main menu.");
-      Console.ReadKey();
-      Lessons.Projmenu();
     }
 }
